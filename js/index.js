6170e81dd70e8266f19298cb7db2e1a29bec00f7
Ricardo Cardona Lafon <cardonalafon@gmail.com>
2017-09-22 10:41:57 -0300

    Version 3
    Carga de las listas desplegables realizadas con éxito. Próximo objetivo lograr mostrar todos los inmuebles.

--- js/index.js
+++ js/index.js
@@ -1,61 +1,55 @@
-/*$(document).ready(function() {
-    $('select').material_select(); 
-});*/
-
 function inicializarSlider(){
-$('#rangoPrecio').ionRangeSlider({
-type: 'double',
-grid: false,
-min: 0,
-max: 100000,
-from: 200,
-to: 80000,
-prefix: '$',
+    $('#rangoPrecio').ionRangeSlider({
+    type: 'double',
+    grid: false,
+    min: 0,
+    max: 100000,
+    from: 200,
+    to: 80000,
+    prefix: '$',
 
-});
+    });
 }
 
 function loadSelects(){
 
 $.ajax(
-{
-url:'loadCities.php',
-type:'GET',
-data:{},
-success: function (data) {
+    {
+        url:'loadCities.php',
+        type:'GET',
+        data:{},
+        success: function (data) {
+            var SeelctCiudades = $("#selectCiudad");
+            SeelctCiudades.html("");
+            SeelctCiudades.append( "<option value='' disabled>Elige una ciudad</option>");
+            SeelctCiudades.append( "<option value='' selected >Todas</option>");
+            jQuery.each(JSON.parse(data), function (i,val) {
+                SeelctCiudades.append("<option value=''>"+ val +"</option>")
+            });
+            SeelctCiudades.material_select();
+        }
 
-var SeelctCiudades = $("#selectCiudad");
-SeelctCiudades.html("");
-SeelctCiudades.append( "<option value='' disabled>Elige una ciudad</option>");
-SeelctCiudades.append( "<option value='' selected >Todas</option>");
-jQuery.each(JSON.parse(data), function (i,val) {
-SeelctCiudades.append("<option value=''>"+ val +"</option>")
-});
-SeelctCiudades.material_select();
-}
-
-}
+    }
 
 );
 
 $.ajax(
-{
-url:'loadTypes.php',
-type:'GET',
-data:{},
-success: function (data) {
+    {
+        url:'loadTypes.php',
+        type:'GET',
+        data:{},
+        success: function (data) {
+            var SeelctTypes = $("#selectTipo");
+            SeelctTypes.html("");
+            SeelctTypes.append( "<option value='' disabled>Elige un tipo</option>");
+            SeelctTypes.append( "<option value='' selected >Todos</option>");
+            jQuery.each(JSON.parse(data), function (i,val) {
+            SeelctTypes.append("<option value=''>"+ val +"</option>")
+            });
+            SeelctTypes.material_select();
+        }
 
-var SeelctTypes = $("#selectTipo");
-SeelctTypes.html("");
-SeelctTypes.append( "<option value='' disabled>Elige un tipo</option>");
-SeelctTypes.append( "<option value='' selected >Todos</option>");
-jQuery.each(JSON.parse(data), function (i,val) {
-SeelctTypes.append("<option value=''>"+ val +"</option>")
-});
-SeelctTypes.material_select();
-}
-
-}
+    }
 
 );
 
@@ -63,43 +57,43 @@ SeelctTypes.material_select();
 
 function search(all) {
 
-var search = $("#serachResult");
+    var search = $("#serachResult");
 
-search.html("");
+    search.html("");
 
-$.ajax(
-{
-url:'search.php',
-type:'GET',
-data:{ showAll:all,
-city:$("#selectCiudad option:selected").text(),
-type:$("#selectTipo option:selected").text(),
-minValue:$("#rangoPrecio").data("from"),
-maxValue:$("#rangoPrecio").data("to")
-},
+    $.ajax(
+        {
+        url:'search.php',
+        type:'GET',
+        data:{ showAll:all,
+        city:$("#selectCiudad option:selected").text(),
+        type:$("#selectTipo option:selected").text(),
+        minValue:$("#rangoPrecio").data("from"),
+        maxValue:$("#rangoPrecio").data("to")
+        },
 
-success: function (data) {
-search.html(data);
-}
+        success: function (data) {
+        search.html(data);
+        }
 
-}
+        }
 
-);
+    );
 
-}
+    }
 
 $(document).ready(function() {
 
-loadSelects();
-inicializarSlider();
+    loadSelects();
+    inicializarSlider();
 
-$("#formulario").on("submit",function (e) {
-e.preventDefault();
-search(false);
-});
+    $("#formulario").on("submit",function (e) {
+        e.preventDefault();
+        search(false);
+    });
 
-$("#mostrarTodos").click(function () {
-search(true);
-});
+    $("#mostrarTodos").click(function () {
+        search(true);
+    });
 
 });
